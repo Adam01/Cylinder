59787d6dfb95940fbab7d742ea011b5f30adbd29
Adam01 <adamguy93@gmail.com>
2015-04-08 00:46:28 +0100

    Redone callback system: -BoundObserver keeps track of what it subscribes to -Ensures references are broken when widgets are removed
    Added Widgets:
    -CodeMirrorWidget
    -EntityField
    -ContextMenu (not used yet)
    
    CodeMirrorWidget is self explainatory, EntityField contains widgets for holding information belonging to a file system entity. Also contains a factory to return a suitable widget based on the entity attribute.
    
    Boostrap'd it up, using boostrap theme for jquery UI.

--- generate_all.py
+++ generate_all.py
@@ -1,9 +1,11 @@
 import sys
 import os
 
-import clean_js
+#import clean_js
 
 script_list = list()
+scss_list = list()
+html_list = list()
 
 main_script = os.path.abspath( os.path.join("rapyd", "Cylinder.pyj") )
 
@@ -20,28 +22,31 @@ for root, dirs, files in os.walk("rapyd"):
         if file.lower().endswith(".pyj"):
             infile = os.path.abspath( os.path.join(root, file) )
             outfile = os.path.abspath( os.path.join(outdir, file[:-4]+".js") )
-            #cmd = 'rapydscript -p --screw-ie8 "%s" -o "%s"' % (infile, outfile)
-            if infile != main_script:
-                script_list.append(infile)
+
+            if not os.path.exists(outfile) or os.path.getmtime(outfile) < os.path.getmtime(infile):
+                cmd = 'rapydscript -p --screw-ie8 "%s" -o "%s"' % (infile, outfile)
         elif file.lower().endswith(".scss"):
             infile = os.path.abspath( os.path.join(root, file) )
             outfile = os.path.abspath( os.path.join(outdir, file[:-5]+".css") )
-            cmd = 'node-sass "%s" "%s"' % (infile, outfile)
+            if not os.path.exists(outfile) or  os.path.getmtime(outfile) < os.path.getmtime(infile):
+                cmd = 'node-sass "%s" "%s"' % (infile, outfile)
         else:
             infile = os.path.abspath( os.path.join(root, file) )
             outfile = os.path.abspath( os.path.join(outdir, file) )
-            if sys.platform.startswith("win"):
-                cmd = 'copy /Y "%s" "%s"' % (infile, outfile)
-            elif sys.platform == "linux":
-                #TODO
-                pass
+            if not os.path.exists(outfile) or os.path.getmtime(outfile) < os.path.getmtime(infile):
+                    if sys.platform.startswith("win"):
+                        cmd = 'copy /Y "%s" "%s"' % (infile, outfile)
+                    elif sys.platform == "linux":
+                        #TODO
+                        pass
 
         if len(cmd) > 0:
             print cmd
-            os.system(cmd)
+            ret = os.system(cmd)
+            if ret != 0:
+                exit()
 
-script_list.append(main_script)
-
-cmd = 'rapydscript -p --screw-ie8 "%s" -o ./js/Cylinder.js' % ('" "'.join(script_list))
-print cmd
-os.system(cmd)
+#script_list.append(main_script)
+#cmd = 'rapydscript -p --screw-ie8 "%s" -o ./js/Cylinder.js' % ('" "'.join(script_list))
+#print cmd
+#os.system(cmd)
